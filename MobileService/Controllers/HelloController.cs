939863f5757b46efbb39cac6ba5ceb92f140fb62
gaob <nightfir@mail.com>
2015-02-08 12:31:00 +0800

    BadRequestResponse handling.

--- MobileService/Controllers/HelloController.cs
+++ MobileService/Controllers/HelloController.cs
@@ -19,16 +19,27 @@ namespace CustomAPIMobileService.Controllers
         /// <returns>HttpResponseMessage.</returns>
         public HttpResponseMessage Post([FromBody]dynamic payload)
         {
-            if (payload != null && payload.msg != null)
+            try
             {
+                if (payload == null ||
+                    payload.one == null || payload.two == null || payload.three == null ||
+                    payload.four == null || payload.five == null || payload.six == null ||
+                    payload.seven == null || payload.eight == null || payload.nine == null)
+                {
+                    throw new KeyNotFoundException();
+                }
+
                 return Request.CreateResponse(HttpStatusCode.OK,
-                    new
-                    {
-                        move = "three",
-                        winner = "inconclusive"
-                    });
+                                                new
+                                                {
+                                                    move = "four",
+                                                    winner = "inconclusive"
+                                                });
             }
-            return Request.CreateResponse(HttpStatusCode.OK, new { move = "three", winner = "inconclusive" });
+            catch (Exception ex)
+            {
+                return Request.CreateBadRequestResponse();
+            }
         }
 
         /// <summary>
