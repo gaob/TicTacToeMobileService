000bb8702508e56d69adce82daf6d294a52d2fff
gaob <nightfir@mail.com>
2015-02-08 11:17:55 +0800

    Update default response.

---
+++ MobileService/Controllers/HelloController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace CustomAPIMobileService.Controllers
+{
+    public class HelloController : ApiController
+    {
+        public ApiServices Services { get; set; }
+
+        /// <summary>
+        /// Takes in the input message, adds date and time and returns it.
+        /// </summary>
+        /// <param name="payload">The payload.</param>
+        /// <returns>HttpResponseMessage.</returns>
+        public HttpResponseMessage Post([FromBody]dynamic payload)
+        {
+            if (payload != null && payload.msg != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK,
+                    new
+                    {
+                        move = "three",
+                        winner = "inconclusive"
+                    });
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, new { move = "three", winner = "inconclusive" });
+        }
+
+        /// <summary>
+        /// Gets the date time
+        /// </summary>
+        /// <returns>HttpResponseMessage.</returns>
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,
+                new
+                {
+                    message = "Hello World! (GET) " + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")
+                });
+        }
+
+    }
+}
